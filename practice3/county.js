3010e5524ea0e75701699aef863384cbf258f49e
MANIVENKATRAJ M <127532509+mp143mani@users.noreply.github.com>
2023-04-29 21:22:12 +0530

    up2

---
+++ practice3/county.js
@@ -0,0 +1,28 @@
+//Final Code for the Rest countries
+var container=document.createElement("div");
+container.className="container";
+
+var row=document.createElement("div");
+row.classList.add("row","m-3");
+
+container.append(row);
+
+var res=fetch("https://restcountries.com/v3.1/all")
+res.then((data)=>data.json()).then((data1)=>foo(data1));
+
+function foo(data1){
+    for(var i=0;i<data1.length;i++){
+        row.innerHTML+=`
+        <div class="col-md-4">
+        <div class="card border-primary mb-3 " style="width: 18rem;">
+        <img src="${data1[i].flags.svg}" class="card-img-top" alt="country flags">
+          <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+      </div>
+      `;
+      document.body.append(container);
+    }
+}
--- practice3/script1.js
+++ practice3/script1.js
@@ -1,5 +1,43 @@
-var res=fetch("https://restcountries.com/v3.1/all");
-var final =res.then((data)=>data.json());
-console.log(final);
-var finaldata=final.then((data)=>data.json());
-console.log(dfinal)
+// -------------------------------------------------example1------------------------------------------------------
+// var res=fetch("https://restcountries.com/v3.1/all");
+// console.log(res);
+// var final=res.then((data)=>data.json());
+// console.log(final);
+// final.then((data)=>console.log(data)).catch((error)=>console.log(error))
+
+
+
+
+//-----------------example 2-----------------------------------------------------------------
+//Question 1:
+//Print the countries name and capital in console
+//Print the flags of each countries in console
+
+
+//Print the countries name and capital in console
+// var res=fetch("https://restcountries.com/v3.1/all")
+// .then((data)=>data.json()).then((data1)=>{
+//     console.log(data1);
+//     for(var i=0;i<data1.length;i++){
+//         console.log(`Country name:${data1[i].name.common} Capital:${data1[i].capital}`);
+//     }
+// });
+
+var res=fetch("https://restcountries.com/v3.1/all")
+.then((data)=>data.json()).then((data1)=>{
+    console.log(data1);
+    for(var i=0;i<data1.length;i++){
+        console.log(`Country name:${data1[i].name.common} Capital:${data1[i].capital}`);
+        console.log(`Country flag:${data1[i].name.common} Flag:${data1[i].flags.svg}`);
+        console.log(`Country symbol:${data1[i].Currencies.common} Currencies:${data1[i].symbol}`);
+    }
+});
+console.log("countryname"+data.name)
+
+
+
+
+
+///----------------------example3-------------------------
+// var res=fetch("https://restcountries.com/v3.1/all")
+// .then((data)=>data.json()).then((data1)=>console.log(data1));
